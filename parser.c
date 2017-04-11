4ee0b219c09da7f08c457ff16cf8671def239a53
Facu Velasco <velascofacundo@gmail.com>
2017-04-11 19:07:11 -0300

    finish prog ready to test

--- parser.c
+++ parser.c
@@ -1,72 +1,69 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-// #include "parser.h" ??
+#include "parser.h"
 
-// hay un int main()??
+int PARS_Crear(T_Parser * parser, char * archivo, char delimitador, char escape) {
 
-int PARS_Crear(TParser * parser, char * archivo, char delimitador, char escape) {
-
-  // abro el archivo en modo lectura
+  /* abro el archivo en modo lectura */
   parser->archivo = fopen(archivo, "r");
   if (parser->archivo == NULL){
     return 0;
   }
-  
+
   parser->delimitador = delimitador;
   parser->escape = escape;
-  
+
   return 1;
 }
 
-int PARS_ObtLinea(TParser * parser) {
+int PARS_ObtLinea(T_Parser * parser) {
 
   char linea[200];
   char * campo;
-  
+
   if (fgets(linea, 200, parser->archivo) != NULL) {
-  
-    ObtProxCampo(linea, campo);
-    while(campo){
-      parser->campo = campo; // hace falta un strcpy?
-      ObtProxCampo(linea, campo);
+
+    while(ObtProxCampo(parser, linea, campo)){
+      parser->(++campos) = campo;
+      /* parser->campos++; */
     }
     return 1;
   }
   return 0;
 }
 
-int PARS_ObtCampo(TParser * parser, int N, char * campo) {
+int PARS_ObtCampo(T_Parser * parser, int N, char * campo) {
 
-  // Compruebo que pida un campo valido
-  if (N > PARS_ObtQCampos(parser)) { 
+  /* Compruebo que pida un campo valido */
+  if (N > PARS_ObtQCampos(parser)) {
     return 0;
   }
   strcpy(campo, parser->campos[N]);
   return 1;
 }
 
-int PARS_ObtQCampos(TParser * parser) {
-  return sizeof(parser->campos) / sizeof(parser->campos[0])
-  /*
-  int nCampos = 1;
-  while ( ++parser.campos != '/0') {
-    nCampos++;
-  }
-  return nCampos;
-  */
+int PARS_ObtQCampos(T_Parser * parser) {
+  return sizeof(parser->campos) / sizeof(parser->campos[0]);
 }
 
-void PARS_Destruir(TParser * parser) {
-  parser->File = NULL;
+void PARS_Destruir(T_Parser * parser) {
+  parser->archivo = NULL;
   parser->delimitador = NULL;
   parser->escape = NULL;
 
   fclose(parser->archivo);
 }
 
-int ObtProxCampo(char * linea, char * campo) {
-  // leer hasta la coma a menos que este el escape antes
-
-  return 0; // si no consigue campo devuelvo 0
+int ObtProxCampo(T_Parser * parser, char * linea, char * campo) {
+  /* Me fijo que la linea haya cortado en delimitador y no en el final de la linea */
+  if (*(++linea) == parser->delimitador) {
+    /* leer hasta el delimitador a menos que este el escape antes */
+    while ((*(++linea) != parser->delimitador || *(++linea) != '\0') || *(linea--) == parser->escape) {
+      *(++campo) = *(++linea);
+    }
+    *campo = '\0';
+    return 1;
+  }
+  return 0;
 }
--- parser.h
+++ parser.h
@@ -3,11 +3,11 @@
 
 /*  ESTRUCTURA  */
 
-typedef struct T_Parcer {
+typedef struct T_Parser {
   FILE * archivo;
   char delimitador, escape;
   char ** campos;
-} T_Parcer;
+} T_Parser;
 
 /*  FUNCIONES  */
 
@@ -16,30 +16,30 @@ PRE: "parser" no creado, "delimitador" es un caracter no nulo,
   "escape" es un caracter no nulo y distinto de delimitador.
 POST: Si no hubo error parser creado y devuelve 1, si no devuelve 0.
 */
-int PARS_Crear(TParser * parser, char * archivo, char delimitador, char escape);
+int PARS_Crear(T_Parser * parser, char * archivo, char delimitador, char escape);
 
 /*
 PRE: "parser" creado.
 POST: Si pudo obtener una linea devuelve 1 (no hubo errores),
   si no devuelve 0 (no hay mas lineas para leer en el archivo).
 */
-int PARS_ObtLinea(TParser * parser);
+int PARS_ObtLinea(T_Parser * parser);
 
 /*
 PRE: "parser" creado, "linea" obtenida.
 POST: Si pudo obtener campo devuelve 1 (no hubo errores) y el campo obtenido,
   si no devuelve 0 (no existe el n-esimo campo pedido).
 */
-int PARS_ObtCampo(TParser * parser, int N, char * campo);
+int PARS_ObtCampo(T_Parser * parser, int N, char * campo);
 
 /*
 PRE: "parser" creado, "linea" obtenida.
 POST: Devuelve la cantidad de campos de la ultima linea obtenida.
 */
-int PARS_ObtQCampos(TParser * parser);
+int PARS_ObtQCampos(T_Parser * parser);
 
 /*
 PRE: "parser" creado.
 POST: "parser" destruido.
 */
-void PARS_Destruir(TParser * parser);
+void PARS_Destruir(T_Parser * parser);
